f08b1b95e665437d5651838918d74d3340dcbfea
ipekg <goekhan.ipek@tipico.com>
2021-10-23 03:48:07 +0200

    WIP initial commit before building planets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="solar-system">
+      <div className="planets sun">sun</div>
+      <div className="planets mercury"><span className="circle"></span>mercury</div>
+      <div className="planets venus"><span className="circle"></span>venus</div>
+      <div className="planets earth"><span className="circle"></span>earth</div>
+      <div className="planets mars"><span className="circle"></span>mars</div>
+      <div className="planets jupiter"><span className="circle"></span>jupiter</div>
+      <div className="planets saturnus"><span className="circle"></span>saturnus</div>
+      <div className="planets uranus"><span className="circle"></span>uranus</div>
+      <div className="planets neptune"><span className="circle"></span>neptune</div>
+      <div className="planets pluto"><span className="circle"></span>pluto</div>
     </div>
   );
 }
